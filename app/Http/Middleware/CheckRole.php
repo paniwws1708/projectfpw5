f1eaf56392e5620b18b3dce16d18ac728aee3e46
stevani1708 <stevaniputri1708@gmail.com>
2026-09-19 10:15:00 +0700

    Middleware

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+{
+    if (! $request->user() || ! in_array($request->user()->role, $roles)) {
+        
+        return response()->view('errors.403', [], 403);
+    }
+
+    return $next($request);
+}
+}
